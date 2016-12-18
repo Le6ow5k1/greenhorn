169f722a9a1f5d164d847137d5ca3f7656182685
Konstantin Lazarev <le6oww5k1@gmail.com>
2016-12-18 19:26:42 +0500

    Remove handler tests

--- test/greenhorn/handler_test.clj
+++ test/greenhorn/handler_test.clj
@@ -1,14 +1,3 @@
 (ns greenhorn.handler-test
   (:require [clojure.test :refer :all]
-            [ring.mock.request :as mock]
-            [greenhorn.handler :refer :all]))
-
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+            [ring.mock.request :as mock]))
